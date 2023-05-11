5ca1beca5c4ada8fe0834cdfccff1d84b576a682
John <johnkaranjairungu@gmail.com>
2023-05-11 18:42:44 +0300

    removed unnecesary code,  changed image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import MemeList from "./components/MemeList";
@@ -8,14 +8,6 @@ import "./components/App.css";
 
 
 function App() {
-  const [memes, setMemes] = useState([]);
-  useEffect  (() => {
-    fetch("https://meme-api.com/gimme/50")
-      .then((response) => response.json())
-      .then((data)=> data.memes)
-      .then(memes => setMemes(memes))
-  }, []);
-
   return(
     <div>
       <NavBar/>
